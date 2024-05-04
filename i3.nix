487741441c30cb0869e820d046fec38a3d582535
Michael Daffin <michael@daffin.io>
2024-05-04 23:16:41 +0100

    more i3 config

--- i3.nix
+++ i3.nix
@@ -45,6 +45,8 @@
         "${mod}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun -modi drun,window,calc,run,power-menu:rofi-power-menu";
         # "${mod}+x" = "exec sh -c '${pkgs.maim}/bin/maim -s | xclip -selection clipboard -t image/png'";
         "${mod}+Shift+l" = "exec sh -c '${pkgs.i3lock}/bin/i3lock -c 444444 & sleep 5 && xset dpms force off'";
+        "${mod}+Ctrl+x" = "--release exec --no-startup-id ${pkgs.xorg.xkill}/bin/xkill";
+
         "XF86AudioRaiseVolume" = "exec --no-startup-id wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+";
         "XF86AudioLowerVolume" = "exec --no-startup-id wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%-";
         "XF86AudioMute" = "exec --no-startup-id wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
@@ -55,29 +57,50 @@
         "XF86MonBrightnessUp" = "exec brightnessctl set 2%+";
         "XF86MonBrightnessDown" = "exec brightnessctl set 2%-";
 
-        # # Focus
-        # "${mod}+j" = "focus left";
-        # "${mod}+k" = "focus down";
-        # "${mod}+l" = "focus up";
-        # "${mod}+semicolon" = "focus right";
+        # Focus
+        "${mod}+Left" = "focus left";
+        "${mod}+j" = "focus left";
+        "${mod}+Down" = "focus down";
+        "${mod}+k" = "focus down";
+        "${mod}+Up" = "focus up";
+        "${mod}+l" = "focus up";
+        "${mod}+Right" = "focus right";
+        "${mod}+semicolon" = "focus right";
 
-        # # Move
-        # "${mod}+Shift+j" = "move left";
-        # "${mod}+Shift+k" = "move down";
-        # "${mod}+Shift+l" = "move up";
-        # "${mod}+Shift+semicolon" = "move right";
+        # Move
+        "${mod}+Shift+Left" = "move left";
+        "${mod}+Shift+j" = "move left";
+        "${mod}+Shift+Down" = "move down";
+        "${mod}+Shift+k" = "move down";
+        "${mod}+Shift+Up" = "move up";
+        "${mod}+Shift+l" = "move up";
+        "${mod}+Shift+Right" = "move right";
+        "${mod}+Shift+semicolon" = "move right";
 
         # # My multi monitor setup
         # "${mod}+m" = "move workspace to output DP-2";
         # "${mod}+Shift+m" = "move workspace to output DP-5";
-      };
 
-      # bars = [
-      #   {
-      #     position = "bottom";
-      #     statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${./i3status-rust.toml}";
-      #   }
-      # ];
+        "${mod}+Tab" = "focus parent";
+        "${mod}+Shift+Tab" = "focus child";
+
+        "${mod}+Ctrl+Right" = "workspace next";
+        "${mod}+Ctrl+Left" = "workspace prev";
+
+        # move workspace to different screens
+        "${mod}+Ctrl+Shift+Left" = "move workspace to output left";
+        "${mod}+Ctrl+Shift+Right" = "move workspace to output right";
+
+        "${mod}+Mod1+Left" = "resize shrink width 5 px or 5 ppt";
+        "${mod}+Mod1+Down" = "resize grow height 5 px or 5 ppt";
+        "${mod}+Mod1+Up" = "resize shrink height 5 px or 5 ppt";
+        "${mod}+Mod1+Right" = "resize grow width 5 px or 5 ppt";
+
+        "${mod}+Mod1+Shift+Left" = "resize shrink width 1 px or 1 ppt";
+        "${mod}+Mod1+Shift+Down" = "resize grow height 1 px or 1 ppt";
+        "${mod}+Mod1+Shift+Up" = "resize shrink height 1 px or 1 ppt";
+        "${mod}+Mod1+Shift+Right" = "resize grow width 1 px or 1 ppt";
+      };
     };
   };
 }
