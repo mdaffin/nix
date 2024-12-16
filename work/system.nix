44d5e046327ba0321636ee3f589ed4c0dcc49f89
Michael Daffin <michael.daffin@starlingbank.com>
2024-12-16 17:04:10 +0000

    Move packages to system for work

--- work/system.nix
+++ work/system.nix
@@ -4,11 +4,73 @@
 {
   system = {
     stateVersion = 5;
-    # defaults.smb.NetBIOSName = config.networking.hostName;
-
-    # Add ability to used TouchID for sudo authentication
-    # security.pam.enableSudoTouchIdAuth = true;
   };
-  programs.zsh.enable = true;
 
+  programs.zsh.enable = true;
+  programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+  };
+
+  environment.systemPackages = with pkgs; [ 
+    virtualenv
+    alacritty
+    wezterm
+    awscli2
+    bottom
+    carapace
+    coreutils
+    diffr
+    delta
+    rustup
+    cargo-outdated
+    eza
+    fd
+    fzf
+    gh
+    git
+    tailwindcss
+    templ
+    gnupg
+    darwin.apple_sdk.frameworks.Security
+    libgit2
+    openssl
+    openssl.dev
+    perl
+    kotlin-language-server
+    darwin.apple_sdk.frameworks.AppKit
+    libiconv
+    pkg-config
+    go
+    gopls
+    oras
+    trivy
+    google-cloud-sdk
+    grml-zsh-config
+    golangci-lint
+    helix
+    jfrog-cli
+    jq
+    yq
+    just
+    shellcheck
+    k9s
+    kdash
+    kubernetes-helm
+    kustomize
+    nushell
+    pinentry_mac
+    python3
+    ripgrep
+    nerd-fonts.fira-code
+    nerd-fonts.fira-mono
+    sd
+    skhd
+    starship
+    terraform
+    terraform-lsp
+    watchexec
+    yubikey-manager
+    zsh
+  ];
 }
